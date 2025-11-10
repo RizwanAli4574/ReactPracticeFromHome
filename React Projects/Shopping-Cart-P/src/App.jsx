cbf46d2a49155c45f61338519125615cd4d7fa01
Rizwan Ali <rizwanali4579897@gmail.com>
2025-11-10 23:50:42 +0500

    Some Work on Shopping Cart Project

--- React Projects/Shopping-Cart-P/src/App.jsx
+++ React Projects/Shopping-Cart-P/src/App.jsx
@@ -1,12 +1,33 @@
+import { BrowserRouter , Routes , Route, NavLink } from "react-router-dom"
+import Home from "./Pages/Home"
+import Cart from "./Pages/CartPage"
+
+function Navbar(){
+ return (
+   <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <NavLink className="navbar-brand fw-bold" to="/">🛒 MyShop</NavLink>
+        <div>
+          <NavLink className="nav-link d-inline me-3 text-white" to="/">Home</NavLink>
+          <NavLink className="nav-link d-inline text-white" to="/cart">Cart (2)</NavLink>
+        </div>
+      </div>
+    </nav>
+ )
+}
 
 
 function App() {
   
 
   return (
-    <>
-   <h1>Hello Word</h1>
-    </>
+   <BrowserRouter>
+    < Navbar />
+    <Routes>
+       <Route path="/" element={<Home />}/>
+       <Route path="/cart" element={<Cart />}/>
+    </Routes>
+   </BrowserRouter>
   )
 }
 
---
+++ React Projects/Shopping-Cart-P/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import ProductList from "../Components/ProdectList"
+
+
+export default function Home(){
+
+    return (
+         <div className="container mt-4">
+      <h1 className="mb-4">Shop Products</h1>
+         < ProductList />
+    </div>
+    )
+
+}
