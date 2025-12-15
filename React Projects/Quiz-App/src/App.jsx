c7971178bbf2b06c916b7cef237c2fee9b4e18b7
Rizwan Ali <rizwanali4579897@gmail.com>
2025-12-15 23:41:05 +0500

    Some work and creat setup

--- React Projects/Quiz-App/src/App.jsx
+++ React Projects/Quiz-App/src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter , Routes , Route } from "react-router-dom"
+import { Suspense , lazy } from "react"
+import Home from "./Pages/Home"
+import Quiz from "./Pages/Quiz"
+import LeaderBoard from "./Pages/LeaderBoard"
+const Result = lazy( () => import("./Pages/Result"))
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <BrowserRouter>
+      <Suspense fallback={<h2 className="text-center mt-5">Loading...</h2>}>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/quiz" element={<Quiz/>}/>
+          <Route path="/result" element={<Result/>}/>
+          <Route path="/leaderboard" element={<LeaderBoard/>}/>
+        </Routes>
+      </Suspense>
+     </BrowserRouter>
     </>
   )
 }
