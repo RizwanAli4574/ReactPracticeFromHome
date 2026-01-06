0685b38950f0cdb4f568d8036919cc7a98c04e92
Rizwan Ali <rizwanali4579897@gmail.com>
2026-01-06 23:25:01 +0500

    add css

--- React Practice/Form-Hook/src/Form.jsx
+++ React Practice/Form-Hook/src/Form.jsx
@@ -4,9 +4,8 @@ import { DevTool } from "@hookform/devtools";
 function Form() {
   const { register, handleSubmit, control, formState } = useForm({
     defaultValues: {
-      name: "Rizwan Ali",
-      email: "rizwan@gmail.com",
-      age: 20,
+      name: "",
+      email: "",
       social: {
         facebook: "",
         twitter: "",
@@ -24,81 +23,95 @@ function Form() {
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="name">Name </label> <br />
-        <input
-          type="text"
-          id="name"
-          {...register("name", {
-            required: "Name is required",
-          })}
-        />
-        {errors.name && <p className="error">{errors.name?.message}</p>}
-        <br /> <br />
-        <label htmlFor="email">Email </label> <br />
-        <input
-          type="email"
-          id="email"
-          {...register("email", {
-            required: "Email is required",
-            pattern: {
-              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-              message: "Invalid email address",
-            },
-            validate: {
-              notAdminEmail: (value) => {
-                return (
-                  value !== "admin@example.com" ||
-                  "Enter a different email address"
-                );
+        <div className="form-control">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            {...register("name", {
+              required: "Name is required",
+            })}
+          />
+          <p className="error">{errors.name?.message}</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message: "Invalid email address",
               },
-
-              notBlacklistedEmail: (value) => {
-                return (
-                  !value.endsWith("@blacklisted.com") ||
-                  "This domain is not allowed"
-                );
+              validate: {
+                notAdminEmail: (value) => {
+                  return (
+                    value !== "admin@example.com" ||
+                    "Enter a different email address"
+                  );
+                },
+                notBlacklistedEmail: (value) => {
+                  return (
+                    !value.endsWith("@blacklisted.com") ||
+                    "This domain is not allowed"
+                  );
+                },
               },
-            },
-          })}
-        />
-        {errors.email && <p className="error">{errors.email?.message}</p>}
-        <br /> <br />
-        <label htmlFor="age">Age </label> <br />
-        <input
-          type="number"
-          id="age"
-          {...register("age", {
-            required: "Age is required",
-            valueAsNumber: true,
-            min: { value: 18, message: "min 18" },
-            max: { value: 25, message: "max 25" },
-          })}
-        />
-        {errors.age && <p className="error">{errors.age?.message}</p>}
-        <br /> <br />
+            })}
+          />
+          <p className="error">{errors.email?.message}</p>
+        </div>
 
-        
-         <label htmlFor="dob">DOB</label> <br />
-        <input type="date" id="dob" {...register("dob" , {
-          valueAsDate:true
-        })} />
-        <br /> <br />
+        <div className="form-control">
+          <label htmlFor="age">Age</label>
+          <input
+            type="number"
+            id="age"
+            {...register("age", {
+              required: "Age is required",
+              valueAsNumber: true,
+              min: { value: 18, message: "min 18" },
+              max: { value: 25, message: "max 25" },
+            })}
+          />
+          <p className="error">{errors.age?.message}</p>
+        </div>
 
-        <label htmlFor="facebook">Facebook </label> <br />
-        <input type="text" id="facebook" {...register("social.facebook")} />
-        <br /> <br />
+        <div className="form-control">
+          <label htmlFor="dob">DOB</label>
+          <input
+            type="date"
+            id="dob"
+            {...register("dob", {
+              valueAsDate: true,
+              required: "DOB is required"
+            })}
+          />
+          <p className="error">{errors.dob?.message}</p>
+        </div>
 
-        <label htmlFor="twitter">Twitter </label> <br />
-        <input type="text" id="twitter" {...register("social.twitter")} />
-        <br /> <br />
+        <div className="form-control">
+          <label htmlFor="facebook">Facebook</label>
+          <input type="text" id="facebook" {...register("social.facebook")} />
+        </div>
 
-        <label htmlFor="number">primaray Number </label> <br />
-        <input type="number" id="number" {...register("phoneNumbers.0")} />
-        <br /> <br />
+        <div className="form-control">
+          <label htmlFor="twitter">Twitter</label>
+          <input type="text" id="twitter" {...register("social.twitter")} />
+        </div>
 
-         <label htmlFor="number2">Secondary Number </label> <br />
-        <input type="number2" id="number2" {...register("phoneNumbers.1")} />
-        <br /> <br />
+        <div className="form-control">
+          <label htmlFor="number">Primary Number</label>
+          <input type="number" id="number" {...register("phoneNumbers.0")} />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="number2">Secondary Number</label>
+          <input type="number2" id="number2" {...register("phoneNumbers.1")} />
+        </div>
 
         <button type="sumbit">Submit</button>
       </form>
