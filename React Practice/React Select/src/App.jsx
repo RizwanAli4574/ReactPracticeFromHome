e215ca7796605fa301d2b91aade2e6375e6fd2ef
Rizwan Ali <rizwanali4574992@gmail.com>
2026-01-29 13:45:58 +0000

    Some Practice React Select

--- React Practice/React Select/src/App.jsx
+++ React Practice/React Select/src/App.jsx
@@ -1,5 +1,25 @@
+import Select from 'react-select';
+// import { useState } from 'react';
+
 function App() {
-  return <></>;
+
+  // const [fruit, setFruit] = useState(null);
+
+  const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+];
+
+
+  return (
+    <div style={{width: "300px"}}>
+      <h1>React Select Example</h1>
+      <Select  options={options}  onChange={(selected) => console.log(selected)} 
+      isMulti clearValue isDisabled={true}  placeholder="Select your flavors"/>
+      {/* <p>{fruit?.label}</p> */}
+    </div>
+  );
 }
 
 export default App;
