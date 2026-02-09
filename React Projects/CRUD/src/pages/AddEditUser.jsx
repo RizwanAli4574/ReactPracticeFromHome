a6378341f9309729d9a17dff30da7d55f8ed3a5e
Rizwan Ali <rizwanali4579897@gmail.com>
2026-02-09 23:57:11 +0500

    CRUD part 2 start

--- React Projects/CRUD/src/pages/AddEditUser.jsx
+++ React Projects/CRUD/src/pages/AddEditUser.jsx
@@ -1,7 +1,38 @@
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import api from "../api/axios"
+
+import {useForm} from "react-hook-form"
+import {yupResolver} from "@hookform/resolvers/yup"
+
+import userSchema from "../validations/userSchema"
+
+import { InputText } from 'primereact/inputtext';
+import { InputNumber } from 'primereact/inputnumber';
+import { Button } from 'primereact/button';
+        
+
 
 
 function AddEditUser() {
-  return <div>Add Edit User</div>;
+
+  const navigate = useNavigate();
+  
+  const {register, 
+    handleSubmit, 
+    setValue , 
+    formState.{errors}} = useForm({
+      resolver: yupResolver(userSchema)
+    })
+  })
+
+  return (
+    <div>
+      <h2>Add User</h2>
+      
+    </div>
+  )
 }
 
 export default AddEditUser;
